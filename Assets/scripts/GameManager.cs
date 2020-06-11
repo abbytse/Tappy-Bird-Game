d133673ca2d35af45d0d45a116980e54a7a8f8ac
xabbirix <atse5@fordham.edu>
2020-06-11 15:44:34 -0400

    Tap Controller script

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour{
+
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+
+    public static GameManager Instance; 
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countdownPage;
+    public Text scoreText; 
+
+    enum PageState{
+        None,
+        Start,
+        GameOver,
+        Countdown
+    }
+
+    int score = 0; 
+    bool gameOver = false; 
+
+    public bool GameOver{ get{return gameOver;}}
+
+    void Awake(){
+        Instance = this; 
+    }
+
+    void SetPageState(PageState state){
+        switch (state){
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.Countdown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(true);
+                break;
+        }
+    }
+
+    public void ConfirmGameOver(){
+        //activated when replay button is hit 
+
+    }
+
+    public void StartGame(){
+        //activated when play button is hit 
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+    */
+}
---
+++ Assets/scripts/TapController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour{
+    public float tapForce = 10; 
+    public float tiltSmooth = 5; 
+    public Vector3 startPos;
+    Rigidbody2D rigidbody; 
+    Quaternion downRotation; 
+    Quaternion forwardRotation; 
+
+    // Start is called before the first frame update
+    void Start(){
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0,0,-90);
+        forwardRotation = Quaternion.Euler(0,0,35);
+        //rigidbody.simulated = false;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetMouseButtonDown(0)){
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag == "ScoreZone"){
+            //register a score event 
+            //play a sound 
+        }
+        if(col.gameObject.tag == "DeadZone"){
+            //freeze bird 
+            rigidbody.simulated = false;
+            //register a dead event 
+            //play a sound 
+        }
+    }
+}
